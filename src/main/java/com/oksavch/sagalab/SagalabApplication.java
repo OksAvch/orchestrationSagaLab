d44960e8cde161186cba80bf71e954fa00e97038
oksAvch <avchynnikova.oksana@gmail.com>
2025-02-09 16:19:05 -0700

    initial commit

---
+++ src/main/java/com/oksavch/sagalab/SagalabApplication.java
@@ -0,0 +1,13 @@
+package com.oksavch.sagalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagalabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagalabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oksavch/sagalab/SagalabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oksavch.sagalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagalabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
